aa6fbc07b33efae173945d2060d756af15e7c82c
Danil Kutkevich <danil@kutkevich.org>
2021-09-28 21:30:00 +0300

    refactoring

--- sqlcgrep/sqlcgrep.go
+++ sqlcgrep/sqlcgrep.go
@@ -28,44 +28,42 @@ func New(yml []byte, opts ...Option) error {
 		opt(&cfg)
 	}
 
-	for _, pkg := range cfg.Packages {
-		for _, replace := range pkg.Regenerates {
-			f, err := os.OpenFile(filepath.Join(cfg.directory, replace.File), os.O_RDWR, os.ModePerm)
-			if os.IsNotExist(err) {
-				fmt.Fprintf(os.Stderr, "File %q not exist.\n", replace.File)
-				continue
+	for _, reg := range cfg.Regenerates {
+		f, err := os.OpenFile(filepath.Join(cfg.directory, reg.File), os.O_RDWR, os.ModePerm)
+		if os.IsNotExist(err) {
+			fmt.Fprintf(os.Stderr, "File %q not exist.\n", reg.File)
+			continue
 
-			} else if err != nil {
+		} else if err != nil {
+			return fmt.Errorf("regexp compile: %w", err)
+		}
+
+		var regenerates []regenerate.Option
+
+		for _, rep := range reg.Replace.Strings {
+			regenerates = append(regenerates,
+				regenerate.ReplaceString(rep.Match, rep.Replacement),
+			)
+		}
+
+		for _, rep := range reg.Replace.Regexps {
+			re, err := regexp.Compile(rep.Match)
+			if err != nil {
 				return fmt.Errorf("regexp compile: %w", err)
 			}
 
-			var regenerates []regenerate.Option
+			regenerates = append(regenerates,
+				regenerate.ReplaceRegexp(re, rep.Replacement),
+			)
+		}
 
-			for _, rep := range replace.Strings {
-				regenerates = append(regenerates,
-					regenerate.ReplaceString(rep.Match, rep.Replacement),
-				)
-			}
+		if cfg.gofmt != nil {
+			regenerates = append(regenerates, regenerate.WithGofmt(cfg.gofmt))
+		}
 
-			for _, rep := range replace.Regexps {
-				re, err := regexp.Compile(rep.Match)
-				if err != nil {
-					return fmt.Errorf("regexp compile: %w", err)
-				}
-
-				regenerates = append(regenerates,
-					regenerate.ReplaceRegexp(re, rep.Replacement),
-				)
-			}
-
-			if cfg.gofmt != nil {
-				regenerates = append(regenerates, regenerate.WithGofmt(cfg.gofmt))
-			}
-
-			err = regenerate.Pipe(f, f, regenerates...)
-			if err != nil {
-				return fmt.Errorf("regenerate pipe: %s", err)
-			}
+		err = regenerate.Pipe(f, f, regenerates...)
+		if err != nil {
+			return fmt.Errorf("regenerate pipe: %s", err)
 		}
 	}
 
@@ -73,17 +71,17 @@ func New(yml []byte, opts ...Option) error {
 }
 
 type Configuration struct {
-	Packages  []Package `yaml:"packages"`
-	directory string
-	gofmt     *imports.Options
+	Regenerates []Regenerate `yaml:"regenerates"`
+	directory   string
+	gofmt       *imports.Options
 }
 
-type Package struct {
-	Regenerates []Replace `yaml:"regenerates"`
+type Regenerate struct {
+	File    string  `yaml:"file"`
+	Replace Replace `yaml:"replace"`
 }
 
 type Replace struct {
-	File    string   `yaml:"file"`
 	Strings []String `yaml:"strings"`
 	Regexps []Regexp `yaml:"regexps"`
 }
